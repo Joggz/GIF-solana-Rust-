c23a33a521474f0b974bcaa00388c8c10afafa04
laoye_jnr <idrisaboladeo@gmail.com>
2022-02-14 13:31:16 +0100

    :zap: updates gifs counts

--- programs/mytestproject/src/lib.rs
+++ programs/mytestproject/src/lib.rs
@@ -11,6 +11,12 @@ pub mod mytestproject {
         base_account.total_gifs = 0;
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -20,6 +26,13 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+
 }
 
 #[account]
